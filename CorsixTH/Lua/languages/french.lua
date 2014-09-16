4abc7e2681b2b9cf7f2d521e95755728f3f5860e
benckx <benoit.vleminckx@gmail.com>
2014-09-16 11:52:47 +0200

    price impact patch

--- CorsixTH/Lua/languages/french.lua
+++ CorsixTH/Lua/languages/french.lua
@@ -547,6 +547,9 @@ adviser = {
     researcher_needs_desk_3 = "Un chercheur a toujours besoin d'un bureau.",
     nurse_needs_desk_1 = "Chaque infirmière a besoin de son propre bureau.",
     nurse_needs_desk_2 = "Votre infirmière est heureuse d'avoir une pause. Si vous comptez y faire travailler plusieurs personnes en même temps, vous devez leur construire un bureau à chacune.",
+    low_prices = "Certains patients pensent que %s est bon marché. Ils en sont contents, mais on perd de l'argent !",
+    high_prices = "Certains patients pensent que %s est cher. Ils râlent mais au moins vous faites de l'argent !",
+    fair_prices = "Certains patients pensent que %s a un prix normal. Bien bien.",
   },
   cheats = {
     th_cheat = "Félicitations, vous avez débloqué les triches !",
