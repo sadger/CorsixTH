fe55963d1e6309547c9325a96f8eb7b5c2b7b232
benckx <benoit.vleminckx@gmail.com>
2014-09-29 18:52:34 +0200

    goHome() refactoring

--- CorsixTH/Lua/dialogs/patient.lua
+++ CorsixTH/Lua/dialogs/patient.lua
@@ -275,7 +275,7 @@ function UIPatient:goHome()
   end
   self:close()
   self.patient:playSound "sack.wav"
-  self.patient:goHome()
+  self.patient:goHome("kicked")
   self.patient:updateDynamicInfo(_S.dynamic_info.patient.actions.sent_home)
 end
 
